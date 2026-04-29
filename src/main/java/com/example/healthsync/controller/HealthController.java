e769bad2b50ccfc5089a882edc7c7791f5b41274
Allen Martin <realallenmartin@gmail.com>
2026-04-28 19:17:42 -0500

    Phase one of application running
    Application runs
    /health endpoint has been created and is working

---
+++ src/main/java/com/example/healthsync/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.healthsync.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "HealthSync is running";
+    }
+}
